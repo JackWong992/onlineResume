896c7a3157c4cbf0bdb17fd2f75041764795df37
JackWong992 <jackwong992@163.com>
2018-07-23 10:27:50 +0800

    border-bottom left to right

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 setTimeout(function(){
     siteWelcome.classList.remove('active') 
-  },2000)
+},2000)
+
+window.onscroll = function(){
+  if(window.scrollY > 100){
+    topNavBar.classList.add('sticky')
+  
+  }else(
+    topNavBar.classList.remove('sticky')
+  )
+}
